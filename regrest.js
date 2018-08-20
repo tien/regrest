3f364d03c224dc0dfcbfacd58ef33e1829c9fcb0
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-08-20 23:54:00 +1200

    refactor

--- regrest.js
+++ regrest.js
@@ -1,5 +1,9 @@
 function Regrest() {
   this.modes = Object.freeze({ browser: "browser", node: "node" });
+  this.defaultHeader = {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  };
   if (typeof XMLHttpRequest !== "undefined") {
     this.mode = this.modes.browser;
   } else if (typeof require("http") !== "undefined") {
@@ -8,64 +12,19 @@ function Regrest() {
       http: require("http"),
       https: require("https")
     };
+  } else {
+    throw "Regrest does not support this eviroment";
   }
-  this.defaultHeader = {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  };
 }
 
-Regrest.prototype.request = function(requestType, url, body = null, cusHeader) {
+Regrest.prototype.request = function(requestType, url, body, cusHeader) {
   cusHeader = cusHeader || this.defaultHeader;
-  return new Promise((resolve, reject) => {
-    switch (this.mode) {
-      case this.modes.browser:
-        const request = new XMLHttpRequest();
-        request.open(requestType, url, true);
-        Object.entries(cusHeader).forEach(header => {
-          request.setRequestHeader(header[0], header[1]);
-        });
-        request.onload = function() {
-          this.status >= 200 && this.status < 400
-            ? resolve(this.response)
-            : reject(`${this.status} ${this.statusText}`);
-        };
-        request.onerror = function() {
-          reject("connection error");
-        };
-        request.send(body);
-        break;
-      case this.modes.node:
-        const parsedUrl = new URL(url);
-        const options = {
-          host: parsedUrl.host,
-          path: parsedUrl.pathname,
-          method: requestType,
-          headers: cusHeader
-        };
-        const req = this.nodeAdapters[parsedUrl.protocol.slice(0, -1)].request(
-          options,
-          res => {
-            if (res.statusCode >= 200 && res.statusCode < 400) {
-              let rawData = "";
-              res.setEncoding("utf8");
-              res.on("data", chunk => {
-                rawData += chunk;
-              });
-              res.on("end", () => {
-                resolve(rawData);
-              });
-            } else {
-              reject(`${res.statusCode} ${res.statusMessage}`);
-            }
-          }
-        );
-        req.on("error", e => reject(e));
-        body && req.write(body);
-        req.end();
-        break;
-    }
-  });
+  switch (this.mode) {
+    case this.modes.browser:
+      return browserRequest.bind(this)(...arguments);
+    case this.modes.node:
+      return nodeRequest.bind(this)(...arguments);
+  }
 };
 
 Regrest.prototype.get = function(url, cusHeader) {
@@ -77,3 +36,54 @@ Regrest.prototype.post = function(url, data, cusHeader) {
 };
 
 module.exports = new Regrest();
+
+function browserRequest(requestType, url, body, cusHeader) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.open(requestType, url, true);
+    Object.entries(cusHeader).forEach(header => {
+      request.setRequestHeader(header[0], header[1]);
+    });
+    request.onload = function() {
+      this.status >= 200 && this.status < 400
+        ? resolve(this.response)
+        : reject(`${this.status} ${this.statusText}`);
+    };
+    request.onerror = function() {
+      reject("connection error");
+    };
+    request.send(body);
+  });
+}
+
+function nodeRequest(requestType, url, body, cusHeader) {
+  return new Promise((resolve, reject) => {
+    const parsedUrl = new URL(url);
+    const options = {
+      host: parsedUrl.host,
+      path: parsedUrl.pathname,
+      method: requestType,
+      headers: cusHeader
+    };
+    const req = this.nodeAdapters[parsedUrl.protocol.slice(0, -1)].request(
+      options,
+      res => {
+        if (res.statusCode >= 200 && res.statusCode < 400) {
+          let rawData = "";
+          res.setEncoding("utf8");
+          res.on("data", chunk => {
+            rawData += chunk;
+          });
+          res.on("end", () => {
+            resolve(rawData);
+          });
+        } else {
+          reject(`${res.statusCode} ${res.statusMessage}`);
+        }
+      }
+    );
+    req.on("error", e => reject(e));
+    body && req.write(body);
+    req.end();
+  });
+}
