46bc9002fcf8212b214900c93ac87b741b2e9ca0
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-09-09 02:05:54 +1200

    Added TypeScript Def

---
+++ src/regrest.d.ts
@@ -0,0 +1,42 @@
+interface IConfig {
+  method?: string;
+  url?: string;
+  headers?: { [key: string]: string };
+  params?: { [key: string]: any };
+  data?: any | null;
+  maxRedirects?: number;
+}
+
+interface IResponse {
+  status: number;
+  statusText: string;
+  headers: { [key: string]: string };
+  text: string;
+  json: () => any;
+}
+
+interface INetworkError extends Error {
+  name: string;
+  response: IResponse;
+  request: boolean;
+  stack: string;
+}
+
+interface IRegrest {
+  request(config: IConfig): Promise<IResponse>;
+  get(url: string, config?: IConfig): Promise<IResponse>;
+  delete(url: string, config?: IConfig): Promise<IResponse>;
+  head(url: string, config?: IConfig): Promise<IResponse>;
+  options(url: string, config?: IConfig): Promise<IResponse>;
+  post(url: string, data?: any, config?: IConfig): Promise<IResponse>;
+  put(url: string, data?: any, config?: IConfig): Promise<IResponse>;
+  patch(url: string, data?: any, config?: IConfig): Promise<IResponse>;
+}
+
+declare namespace Regrest {
+  type NetworkError = INetworkError;
+}
+
+declare const Regrest: IRegrest;
+
+export = Regrest;
