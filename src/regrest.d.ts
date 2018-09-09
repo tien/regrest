c3c237d73e371ba8569cb04dc0cda960c799f541
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-09-09 13:19:13 +1200

    Refactor type definitions

--- src/regrest.d.ts
+++ src/regrest.d.ts
@@ -1,46 +1,44 @@
-interface IConfig {
-  method?: string;
-  url?: string;
-  headers?: { [key: string]: string };
-  params?: { [key: string]: any };
-  data?: any | null;
-  maxRedirects?: number;
-}
-
-interface IConfigWithUrl {
-  url: string;
-}
-
-interface IResponse {
-  status: number;
-  statusText: string;
-  headers: { [key: string]: string };
-  text: string;
-  json: () => any;
-}
-
-interface INetworkError extends Error {
-  name: string;
-  response: IResponse;
-  request: boolean;
-  stack: string;
-}
-
-interface IRegrest {
-  request(config: IConfig & IConfigWithUrl): Promise<IResponse>;
-  get(url: string, config?: IConfig): Promise<IResponse>;
-  delete(url: string, config?: IConfig): Promise<IResponse>;
-  head(url: string, config?: IConfig): Promise<IResponse>;
-  options(url: string, config?: IConfig): Promise<IResponse>;
-  post(url: string, data?: any, config?: IConfig): Promise<IResponse>;
-  put(url: string, data?: any, config?: IConfig): Promise<IResponse>;
-  patch(url: string, data?: any, config?: IConfig): Promise<IResponse>;
-}
-
 declare namespace Regrest {
-  type NetworkError = INetworkError;
+  export interface IConfig {
+    method?: string;
+    url?: string;
+    headers?: { [key: string]: string };
+    params?: { [key: string]: any };
+    data?: any | null;
+    maxRedirects?: number;
+  }
+
+  export interface IConfigWithUrl extends IConfig {
+    url: string;
+  }
+
+  export interface IResponse {
+    status: number;
+    statusText: string;
+    headers: { [key: string]: string };
+    text: string;
+    json: () => any;
+  }
+
+  export interface INetworkError extends Error {
+    name: string;
+    response: IResponse;
+    request: boolean;
+    stack: string;
+  }
+
+  export interface IRegrest {
+    request(config: IConfigWithUrl): Promise<IResponse>;
+    get(url: string, config?: IConfig): Promise<IResponse>;
+    delete(url: string, config?: IConfig): Promise<IResponse>;
+    head(url: string, config?: IConfig): Promise<IResponse>;
+    options(url: string, config?: IConfig): Promise<IResponse>;
+    post(url: string, data?: any, config?: IConfig): Promise<IResponse>;
+    put(url: string, data?: any, config?: IConfig): Promise<IResponse>;
+    patch(url: string, data?: any, config?: IConfig): Promise<IResponse>;
+  }
 }
 
-declare const Regrest: IRegrest;
+declare const Regrest: Regrest.IRegrest;
 
-export = Regrest;
+export default Regrest;
