cb9e211ef9b709ccbbda6bdfbc8d24803c12b2a1
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-09-14 16:50:35 +1200

    Method should be uppercase

--- src/regrest.js
+++ src/regrest.js
@@ -104,7 +104,7 @@ Regrest.prototype.request = function({
 ["get", "head", "delete", "options"].forEach(
   method =>
     (Regrest.prototype[method] = function(url, config) {
-      return this.request({ ...config, method, url });
+      return this.request({ ...config, method: method.toUpperCase(), url });
     })
 );
 
@@ -118,7 +118,12 @@ Regrest.prototype.request = function({
 ["post", "put", "patch"].forEach(
   method =>
     (Regrest.prototype[method] = function(url, data, config) {
-      return this.request({ ...config, method, url, data });
+      return this.request({
+        ...config,
+        method: method.toUpperCase(),
+        url,
+        data
+      });
     })
 );
 
